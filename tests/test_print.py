347cd0c40784adf508d2f7bf08e6bb211c9fb67d
James Shadow <shadowjames2@gmail.com>
2022-12-20 11:41:14 +0000

    Print.

---
+++ tests/test_print.py
@@ -0,0 +1,17 @@
+print("Print in Code")
+
+print("This line will be printed.")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+#exercise
+print("Task 1!")
+print("Goodby, World!")
+print("Hello, World!")
+
+print("Task 2!")
+print("Hello, I'm James Shadow.")
+print("I'm learning to code to better my future.")
