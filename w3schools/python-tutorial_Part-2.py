ff72d62908e9f703758779c67973b26485e75962
J-R-Shadow <ShadowJames2@gmail.com>
2023-01-26 17:40:46 +0000

    up to if ... else

--- w3schools/python-tutorial_Part-2.py
+++ w3schools/python-tutorial_Part-2.py
@@ -1,4 +1,4 @@
-# w3school.com/Python Up to the end of String Formatting {Part 2}
+# w3school.com/Python Up to the end of String Formatting {Part 2} (1199 line)
 
 # Python Sets
 myset = {"apple", "banana", "cherry"}
@@ -365,3 +365,180 @@ if "model" in thisdict:
     print("Yes, 'model' is one of the keys in the thisdict dictionary")
 
 # Change Dictionary Items
+    # Change Values
+    # You can change the value of a specific item by referring to its key name:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["year"] = 2018
+
+# Update Dictionary
+    # The update() method will update the dictionary with the items from the given argument.
+    # The argument must be a dictionary, or an iterable object with key:value pairs.
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.update({"year": 2020})
+
+# Add Dictionary Items
+    # Adding an item value to the dictionary is done by using a new index key and assigning a value to it:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+# Update Dictionary
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.update({"color": "red"})
+
+# Removing Dictionary Items
+    # There are several ways to remove items from a dictionary:
+# pop()- removes specified key name:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+# popitem() - The popitem() method removes the last inserted item 
+#  (in versions before 3.7, a random item is removed instead):
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+# del - The del keyword removes the item with the specified key name:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+"""
+can also delete a complete dictionary.
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists. 
+"""
+
+# The clear() method empties the dictionary:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+# Loop Dictionaries.
+    # You can loop through a dictionary by using a for loop.
+    # When looping through a dictionary, the return value are the keys of the dictionary,
+    #  but there are methods to return the values as well.
+for x in thisdict:
+    print(x) # print all key names one by one
+
+for x in thisdict:
+    print(thisdict[x]) # print all values in dictionary one by one
+
+for x in thisdict.values():
+    print(x) # to also return values of a dictionary
+
+for x in thisdict.keys():
+    print(x) # to return the keys of a dictionary
+
+for x, y in thisdict.items():
+    print(x, y) # loop through keys and values by using the items() method
+
+# Copy Dictionaries
+    # You cannot copy a dctionary simply by typing 'dict2 = dict1' because: 
+    #   dict2 will only be a reference to dict1, and changes made in dict1 will automatically be made in dict2.
+    # There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# Another way to make a copy is to use the built-in function dict().
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+# Nested Dictionaries
+    # A dictionary can contain dictionaries, this is called nested dictionaries.
+myfamily = {
+    "child1" : {
+        "name" : "Emily",
+        "year" : 2004
+    },
+    "child2" : {
+        "name" : "Tobias",
+        "year" : 2007
+    },
+    "child3" : {
+        "name" : "Linus",
+        "year" : 2011
+    }
+}
+# Or, if you want to add three dictionaries into a new dictionary:
+child1 = {
+    "name" : "Emily",
+    "year" : 2004
+}
+child2 = {
+    "name" : "Tobias",
+    "year" : 2007
+}
+child3 = {
+    "name" : "Linus",
+    "year" : 2011
+}
+myfamily = {
+    "child1" : child1,
+    "child2" : child2,
+    "child3" : child3
+}
+
+# Dictionary Methods
+"""
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+"""
+
+# Python - if ... else
