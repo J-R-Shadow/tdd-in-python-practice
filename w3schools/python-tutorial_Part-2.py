b272122142bfdea025510c28a054842279a47a46
J-R-Shadow <ShadowJames2@gmail.com>
2023-01-30 17:36:22 +0000

    got to For Loops

--- w3schools/python-tutorial_Part-2.py
+++ w3schools/python-tutorial_Part-2.py
@@ -542,3 +542,160 @@ values()	Returns a list of all the values in the dictionary
 """
 
 # Python - if ... else
+    # Conditions and If statements
+"""
+python supports the usual logical conditions from mathmatics:
+    - Equals: a == b
+    - Not Equals: a != b
+    - Less than: a < b
+    - Less than or equal to: a <= b
+    - Greater than: a > b
+    - Greater than or equal to: a >= b
+These conditions can be used in several ways, most commonly in "if statements" and loops.
+an "if statement" is written by using the 'if' keyword.
+"""
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# In this example we use two variable, a and b , which are used as part of the if statement to test wether b is greater than a. As a is 33, and b is 200, we know
+#  that 200 is greater than 33, and so we print to screen that "b is greater than a".
+
+# Indentation
+"""
+Python relies on indention (whitespace at the beginning of a line) to define scope in the code.
+Other programming languages use curle-brackets for this purpose.
+If statement, without indentation will raise an error:
+a = 33
+b = 200
+if b > a:
+print("b is greater than a")
+"""
+# Elif
+    # The 'elif' keyword is Python's way of saying "if the previousconditions were not true, then try this condition".
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+# In the example ais equal to b, so the first condition is not true, but the 'elif' condition is.
+
+# Else
+    # The 'else' keyword catches anything which isn't caught by the preceding conditions.
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+# In this example a is greater than b, so the first condition is not true, also the 'elif' condition is not true, so we
+    # go to the else condtion and print to screen that "a is greater than b".
+# You can also have an else without the elif:
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# Short Hand If
+    # If you have only one statement to execute, you can put it on the dame line as the if statement.
+if a > b: print("a is greater than b")
+
+# Short Hand If ... Else
+    # If you have only one statement to execute, one for if, and one for else, you can put it all on the same line:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+    # This technique is known as Ternary Operators, or Conditional Expressions.
+# You can also have multiple else statements on the same line:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# And
+    # The 'and' keyword is a logical operator, and is used to combine conditional statements:
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+
+# Or
+    # The 'or' keyword is a logical operator, and is used to combine conditional statemenets:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the conditions is True")
+
+# Not
+    # The not keyword is a logical operator, and is used to reverse the result of the conditional statement:
+a = 33
+b = 200
+if not a > b:
+    print("a is NOT greater than b")
+
+# Nested if
+    # You can have if statements inside if statments, this is called nested if statements.
+x = 41
+
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+
+# The pass statement
+    # if statments cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+a = 33
+b = 200
+
+if b > a:
+    pass
+
+# While Loops
+# Loops
+    # python has two primative loop commands: While and For
+#The While Loop
+    # With the 'while' loop we can execute a set of statements as long as a condtion is true.
+i = 1
+while i < 6:
+    print(i)
+    i += 1  # Remember to increment i or else the loop will continue forever.
+# The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which is set to 1.
+
+# The break statement
+    # With the break statement we can stop the loop even if the while condition is true:
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+# The continue statement
+    # with the continue statement we cn stop the current iteration, and continue with the next:
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+# The else statement
+    # With the else statement we can run a block of code once when the condition no longer is true:
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
+# For Loops
+    
